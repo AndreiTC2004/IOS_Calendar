d781578119693fdbd2a018d1a792fc217491794e
AndreiTC2004 <turica.andrei@gmail.com>
2024-12-29 22:17:46 +0200

    Initial Commit

---
+++ IOS_ClandarTests/IOS_ClandarTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_ClandarTests.swift
+//  IOS_ClandarTests
+//
+//  Created by Andrei Turică on 29.12.2024.
+//
+
+import Testing
+@testable import IOS_Clandar
+
+struct IOS_ClandarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
